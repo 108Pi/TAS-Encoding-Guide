14396cc8f73feead815cce876d3f03ab98e226ea
108Pi <55302600+108Pi@users.noreply.github.com>
2024-03-04 13:35:35 -0800

    Delete MergeAVIs.bat

--- MergeAVIs.bat
+++
@@ -1,8 +0,0 @@
-::Merges .avi files based on alphabetical/numeric order 
-@echo off
-(for %%i in (*.avi) do @echo file '%%i') > avilist.txt
-ffmpeg -safe 0 -f concat -i avilist.txt -c copy merged.avi
-del avilist.txt
-echo finished merging AVIs
-pause
-exit
