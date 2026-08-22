6b29f7a4809654c55bef41425e82a8c7a1971fb7
108Pi <55302600+108Pi@users.noreply.github.com>
2026-08-21 19:32:20 -0700

    Fixed merge and encode by slither

--- MergeAndEncode.bat
+++ MergeAndEncode.bat
@@ -1,10 +1,7 @@
 @echo off
-set /p f=upscale factor: 
-(for %%i in (*.avi) do @echo file '%%i') > list.txt
-ffmpeg -safe 0 -f concat -i list.txt -c copy input.avi
-del list.txt
-ffmpeg -i input.avi -c:v libx264 -c:a copy iw*%f%:ih*%f%-sws_flags neighbor output.mkv
-del input.avi
+set /p f=upscale factor:
+(for %%i in (*.avi) do @echo file '%%i') > e2OVejDPt5UYoqLK.txt
+ffmpeg -safe 0 -f concat -i e2OVejDPt5UYoqLK.txt -vf scale=iw*%f%:ih*%f%:flags=neighbor -c:v libx264 -pix_fmt yuv420p -c:a libopus -b:a 256k output.mp4
+del e2OVejDPt5UYoqLK.txt
 echo encoding finished
 pause
-exit
